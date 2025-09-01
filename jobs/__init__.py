99a7d6d9563d0e1c21eeecab309ae2a1d1be7a42
Hung Luu <hung.luu@gmail.com>
2025-09-01 08:48:46 -0500

    New Feature Branch - Day 21 Job Upload

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,7 +1,9 @@
 from nautobot.apps.jobs import register_jobs
 from .hello_jobs_day18 import HelloJobDay18
+from .day21_jobs_upload import FileUpload
 
 
-register_jobs(HelloJobDay18)
+register_jobs(HelloJobDay18, 
+              FileUpload)
 
 
